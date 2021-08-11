e015587c2ad850172841fbb2ef2f96e13912790d
Marta Romanyshyn <50634489+marta12121@users.noreply.github.com>
2021-08-12 00:12:28 +0300

    Add files via upload

---
+++ layout.js
@@ -0,0 +1,8 @@
+ function barFunction() {
+     var x = document.getElementById("navbar-nav");
+     if (x.style.display === "block") {
+         x.style.display = "none";
+     } else {
+         x.style.display = "block";
+     }
+ }
